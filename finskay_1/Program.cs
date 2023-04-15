ea76ff08efae9771e8cf9f205aac2a54984ca42c
gamov <gamov@DESKTOP-6L9HVNF>
2023-04-16 00:37:16 +0300

    Добавьте файлы проекта.

---
+++ finskay_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());  // Ввод числа
+int remainder = num % 2;                        // Остаток
+
+if(num != 0)                                    // Проверка на Маслова
+{
+    switch (remainder)
+    {
+        case 0:Console.Write("Ваше число четное");break;
+        case 1:Console.Write("Ваше число не четное");break;
+    }
+}
+else
+{
+    Console.WriteLine("Вы маслов?");
+}
